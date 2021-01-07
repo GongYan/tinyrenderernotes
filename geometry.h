6113802b2fc538e99b53e0cb809f8447b240cdbf
GongYan <669338805@qq.com>
2021-01-07 21:13:29 +0800

    矩阵运算

--- geometry.h
+++ geometry.h
@@ -1,6 +1,7 @@
 #ifndef __GEOMETRY_H__
 #define __GEOMETRY_H__
 
+#include <vector>
 #include <ostream>
 #include <cmath>
 
@@ -101,5 +102,118 @@ typedef Vec2<int>   Vec2i;
 typedef Vec3<float> Vec3f;
 typedef Vec3<int>   Vec3i;
 
+class Matrix{
+private:
+    std::vector<std::vector<float> > m;
+    int rows, cols;
+public:
+    Matrix(int r = 4, int c = 4):m(std::vector<std::vector<float> >(r, std::vector<float>(c, 0.0f))), 
+        rows(r),cols(c){};
+
+    inline int nrows() { return rows; };
+    inline int ncols() { return cols; };
+
+    static Matrix identity(int dimensions){
+        Matrix mat(dimensions, dimensions);
+        for (int i = 0; i < dimensions; i++)
+        {
+            for (int j = 0; j < dimensions; j++)
+            {
+                mat[i][j] = (i == j ? 1.0f : 0.0f);
+            }
+        }
+        return mat;
+    };
+
+    std::vector<float>& operator[] (const int i){
+        assert(i >= 0 && i < rows);
+        return m[i];
+    };
+    //左向右乘
+    Matrix operator* (const Matrix& a){
+        assert(cols == a.rows);
+        Matrix mat(rows, a.cols);
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < a.cols; j++)
+            {
+                mat.m[i][j] = 0.0f;
+                for(int k = 0; k < cols; k++)
+                {
+                    mat.m[i][j] += mat.m[i][k] * a.m[k][j];
+                }
+            }
+        }
+        return mat;
+    };
+
+    //转置阵
+    Matrix transpose(){
+        Matrix mat(cols, rows);
+        for (int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                mat.m[j][i] = m[i][j];
+            }
+        }
+        return mat;
+    };
+
+    //逆
+    Matrix inverse(){
+        assert(rows==cols);
+        // augmenting the square matrix with the identity matrix of the same dimensions A => [AI]
+        Matrix result(rows, cols*2);
+        for(int i=0; i<rows; i++)
+            for(int j=0; j<cols; j++)
+                result[i][j] = m[i][j];
+        for(int i=0; i<rows; i++)
+            result[i][i+cols] = 1;
+        // first pass
+        for (int i=0; i<rows-1; i++) {
+            // normalize the first row
+            for(int j=result.cols-1; j>=0; j--)
+                result[i][j] /= result[i][i];
+            for (int k=i+1; k<rows; k++) {
+                float coeff = result[k][i];
+                for (int j=0; j<result.cols; j++) {
+                    result[k][j] -= result[i][j]*coeff;
+                }
+            }
+        }
+        // normalize the last row
+        for(int j=result.cols-1; j>=rows-1; j--)
+            result[rows-1][j] /= result[rows-1][rows-1];
+        // second pass
+        for (int i=rows-1; i>0; i--) {
+            for (int k=i-1; k>=0; k--) {
+                float coeff = result[k][i];
+                for (int j=0; j<result.cols; j++) {
+                    result[k][j] -= result[i][j]*coeff;
+                }
+            }
+        }
+        // cut the identity matrix back
+        Matrix truncate(rows, cols);
+        for(int i=0; i<rows; i++)
+            for(int j=0; j<cols; j++)
+                truncate[i][j] = result[i][j+cols];
+        return truncate;
+    };
+
+    friend std::ostream& operator<<(std::ostream& s, Matrix& m){
+        for(int i = 0; i < m.nrows(); i++)
+        {
+            for(int j = 0; j < m.ncols(); j++)
+            {
+                s << m[i][j];
+                if(j < m.ncols() - 1) s << "\t";
+            }
+            s << "\n";
+        }
+        return s;
+    };
+};
 
 #endif
